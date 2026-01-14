0dfe7b60b6a5a78bbb86f7c57360828d3a54aa68
aicxxg <aicxxg@gmail.com>
2026-01-14 10:02:10 +0800

    delete error simulation line in health endpoint

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ app.get('/version', (req, res) => {
 })
 
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (false) throw('error...  ')
   res.send('ok')
 })
 
